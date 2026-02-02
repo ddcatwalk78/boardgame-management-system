50c7c349eafb253f07dfa7e683aee2c7f8ccdece
ddcatwalk78 <ddcawalk78@gmail.com>
2026-02-02 23:49:35 +0000

    middleware to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,9 +2,9 @@ import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 
 // ここでは adapter（Prisma）を含まない設定を使う
-export const { auth: middleware } = NextAuth(authConfig);
+export const { auth: proxy } = NextAuth(authConfig);
 
-export default middleware;
+export default proxy;
 
 export const config = {
   // 認証を適用したいパスを指定
